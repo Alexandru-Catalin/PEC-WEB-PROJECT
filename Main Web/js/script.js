828e52ca81d0728d321a17e2850757bdd37bd8ae
TKCSeow <33025216+TKCSeow@users.noreply.github.com>
2019-03-12 09:59:50 +0000

    Skeleton/base site made
    Navbar, and sections made

---
+++ Main Web/js/script.js
@@ -0,0 +1,52 @@
+$(function() {
+  var topoffset = 62;
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=\\#]:not([href=\\#])').click(
+    function() {
+      if (
+        location.pathname.replace(/^\//, '') ===
+          this.pathname.replace(/^\//, '') &&
+        location.hostname === this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate(
+            {
+              scrollTop: target.offset().top - topoffset + 2
+            },
+            500
+          );
+          return false;
+        } //target.length
+      } //click function
+    }
+  ); //smooth scrolling
+
+  $(window).on('activate.bs.scrollspy', function() {
+    var hash = $('.site-nav')
+      .find('a.active')
+      .attr('href');
+
+      if (hash !== '#page-top') {
+        $('header nav').addClass('inbody');
+      } else {
+        $('header nav').removeClass('inbody');
+      }
+  });
+
+  $('#site-modal').on('show.bs.modal', function(event) {
+    $(this)
+      .find('.modal-content img')
+      .attr('src', $(event.relatedTarget).data('highres'));
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+    $('.navbar a').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+
+});
