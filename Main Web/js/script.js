8ff4a5769c7c629e26d54372cc43f066800ab6a1
TKCSeow <33025216+TKCSeow@users.noreply.github.com>
2019-03-14 13:52:38 +0000

    Added hiding sectioned, adding some animations
    TA 14/3/19 12pm - 2pm

--- Main Web/js/script.js
+++ Main Web/js/script.js
@@ -49,4 +49,12 @@ $(function() {
       $('.navbar-collapse').collapse('hide');
     });
 
+
+
 });
+
+
+function nextSection(id) {
+   var e = document.getElementById(id);
+   e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+}
