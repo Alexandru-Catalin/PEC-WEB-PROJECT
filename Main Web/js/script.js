9871500f7f4fb3fe67594fbc18b7ebf8628c4dd1
TKCSeow <33025216+TKCSeow@users.noreply.github.com>
2019-03-18 00:55:10 +0000

    Updated Section changing, add insulation routing, added some learning features

--- Main Web/js/script.js
+++ Main Web/js/script.js
@@ -49,12 +49,116 @@ $(function() {
       $('.navbar-collapse').collapse('hide');
     });
 
-
+    $('#site-modal').on('show.bs.modal', function(event) {
+      $(this)
+        .find('.modal-content img')
+        .attr('src', $(event.relatedTarget).data('highres'));
+    });
 
 });
 
 
+// function nextSection(id) {
+//    var e = document.getElementById(id);
+//    e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+// }
+var isI = false;
+var isH = false;
+var isO = false;
+
 function nextSection(id) {
    var e = document.getElementById(id);
    e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+
+}
+
+function selectSection(id) {
+
+  //if (isI == true || isH == true || isO == true)
+  {
+
+  }
+  //else
+    {
+      if (id == 'page-insulation' && isI == false)
+      {
+        isI = true;
+        var e = document.getElementById('page-insulation');
+        e.style.display = 'block';
+        hideSections(1);
+      }
+
+      else if (id == 'page-heating' && isH == false)
+      {
+        isH = true;
+        var e = document.getElementById('page-heating');
+        e.style.display = 'block';
+        hideSections(2);
+      }
+
+      else if (id == 'page-other' && isO == false)
+      {
+        isO = true;
+        var e = document.getElementById('page-other');
+        e.style.display = 'block';
+
+        hideSections(3);
+      }
+      else {
+        var e = document.getElementById(id);
+        e.style.display = 'none';
+
+        isI = false;
+        isH = false;
+        isO = false;
+      }
+
+      function hideSections(i)
+      {
+        if (i == 1){
+
+        isH = false;
+        var e = document.getElementById('page-heating');
+        e.style.display = 'none';
+        isO = false;
+        var e = document.getElementById('page-other');
+        e.style.display = 'none';
+      }
+      if (i == 2){
+        isO = false;
+        var e = document.getElementById('page-other');
+        e.style.display = 'none';
+        isI = false;
+        var e = document.getElementById('page-insulation');
+        e.style.display = 'none';
+      }
+        if (i == 3){
+        isI = false;
+        var e = document.getElementById('page-insulation');
+        e.style.display = 'none';
+        isH = false;
+        var e = document.getElementById('page-heating');
+        e.style.display = 'none';
+      }
+      }
+
+    }
+  }
+
+
+  // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("topBtn").style.display = "block";
+  } else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
