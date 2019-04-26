1d40c2fa4263ed994c170058c503b7a9b37e9fd9
TKCSeow <33025216+TKCSeow@users.noreply.github.com>
2019-04-26 11:28:44 +0100

    Changed some colors

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ function selectSection(id) {
       if (id == 'page-insulation' && isI == false)
       {
         isI = true;
-        currentSection = 'IA2';
+        currentSection = 'IA1';
         var e = document.getElementById('page-insulation');
         e.style.display = 'block';
         hideSections(1);
@@ -308,21 +308,21 @@ $(document).ready(function(){
     console.log(currentSection);
     $('#' + currentSection).fadeOut()
   });
-  $(".fadein").click(function(){
-    console.log(currentSection);
-    $('#' + currentSection + 'A').fadeIn();
-  });
-  $(".shrink").click(function(){
-    console.log(currentSection);
-    $('#' + currentSection + 'A').fadeIn();
-  });
+  // $(".fadein").click(function(){
+  //   console.log(currentSection);
+  //   $('#' + currentSection + 'A').fadeIn();
+  // });
+  // $(".shrink").click(function(){
+  //   console.log(currentSection);
+  //   $('#' + currentSection + 'A').fadeIn();
+  // });
 
   //On card hover, do animation on icon
   $('#card1 a').hover(function(){
-    $('#card1').find('i').addClass('w3-spin text-success');
+    $('#card1').find('i').addClass('house');
   },
   function(){
-    $('#card1').find('i').removeClass('w3-spin text-success');
+    $('#card1').find('i').removeClass('house');
 });
 
 //On card hover, do animation on icon
