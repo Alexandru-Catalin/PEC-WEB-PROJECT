c4a845df14d475857816af11d0840ded84dd6224
TKCSeow <33025216+TKCSeow@users.noreply.github.com>
2019-05-01 12:38:17 +0100

    removed top button

--- js/script.js
+++ js/script.js
@@ -104,21 +104,21 @@ function selectSection(id) {
   }
 
   // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    document.getElementById("topBtn").style.display = "block";
-  } else {
-    document.getElementById("topBtn").style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// window.onscroll = function() {scrollFunction()};
+//
+// function scrollFunction() {
+//   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+//     document.getElementById("topBtn").style.display = "block";
+//   } else {
+//     document.getElementById("topBtn").style.display = "none";
+//   }
+// }
+//
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
 
 var before1930 = false;
 var after1930 = false;
